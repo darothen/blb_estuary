1356e9b0e1eed0d6e55db91e78c1f8e0661c9184
Daniel Rothenberg <darothen@mit.edu>
2016-03-02 12:50:44 -0500

    Updating incremental changes

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ from estuary import EstuaryModel, basic_tidal_flow
 from bokeh.io import curdoc
 from bokeh.models import ColumnDataSource, Range1d, CustomJS
 from bokeh.models import BoxAnnotation, VBox, HBox, Slider, Toggle, Button
-from bokeh.plotting import Figure
+from bokeh.models import DataTable, TableColumn
+from bokeh.plotting import Figure, gridplot
 
 # Default model settings - can be made accessible to user!
 model_kwargs = dict(V=1e9, z=5., S_ocean=35., N_ocean=20.,
@@ -21,6 +22,7 @@ model_run_kwargs = dict(dt=1.0, t_end=24*42.)
 colors = [
     'MediumSeaGreen', 'OrangeRed', 'DarkViolet'
 ]
+label_fontsize = "14pt"
 tools = "xwheel_zoom,xpan,reset"
 day_range = Range1d(0, model_run_kwargs['t_end']/24.)
 
@@ -70,6 +72,9 @@ top.line('day', 'S', source=source,
          line_color=colors[0], line_width=3, line_cap='round')
 top.y_range = Range1d(0., 40.)
 top.yaxis.axis_label = "Salinity (g/kg)"
+top.xaxis.axis_label_text_font_size = label_fontsize
+top.yaxis.axis_label_text_font_size = label_fontsize
+
 
 mid = Figure(tools=tools, title=None, x_range=top.x_range,
              toolbar_location=None)
@@ -77,6 +82,9 @@ mid.line('day', 'N', source=source,
          line_color=colors[1], line_width=3, line_cap='round')
 mid.y_range = Range1d(0., 200.)
 mid.yaxis.axis_label = "Nitrate (µmol/L)"
+mid.xaxis.axis_label_text_font_size = label_fontsize
+mid.yaxis.axis_label_text_font_size = label_fontsize
+
 
 bot = Figure(tools=tools, title=None, x_range=top.x_range,
              toolbar_location=None)
@@ -84,6 +92,8 @@ bot.line('day', 'O', source=source,
          line_color=colors[2], line_width=3, line_cap='round')
 bot.y_range = Range1d(0., 1000)
 bot.yaxis.axis_label = "Oxygen (µmol/L)"
+bot.xaxis.axis_label_text_font_size = label_fontsize
+bot.yaxis.axis_label_text_font_size = label_fontsize
 
 # Hypoxic criteria line
 bot.line([SPINUP_DAYS, day_range.end], [HYPO_THRESH, HYPO_THRESH],
@@ -102,7 +112,8 @@ for p in [top, mid, bot]:
     p.renderers.extend([spin_up_box, ])
 
 # Generate multi-panel plot and display
-plots = VBox(top, mid, bot)
+plots = VBox(top, mid, bot, width=800)
+# plots = gridplot([[top,], [mid,], [bot,]])
 
 
 def update_plots():
@@ -117,20 +128,19 @@ def update_plots():
 
     results = run_model(has_tide, river_flow_rate, N_river, G, P)
 
-    source.data['V'] = results['V']
-    source.data['S'] = results['S']
-    source.data['N'] = results['N']
-    source.data['O'] = results['O']
-    source.data['day'] = results['day']
+    # Update internal data handler with latest results/model run output
+    source.data = dict(V=results['V'], S=results['S'],
+                       N=results['N'], O=results['O'],
+                       day=results['day'])
 
-    title_str = "Estuary"
-    comps = []
-    if has_tide:
-        comps += ['tides']
-    if river_flow_rate > 0:
-        comps += ['river']
-    extra_str = " with " + " and ".join(comps)
-    top.title = title_str + extra_str
+    # title_str = "Estuary"
+    # comps = []
+    # if has_tide:
+    #     comps += ['tides']
+    # if river_flow_rate > 0:
+    #     comps += ['river']
+    # extra_str = " with " + " and ".join(comps)
+    # top.title = title_str + extra_str
 
     # Reset plot ranges if necessary
     # TODO: This seems to be an open bug in bokeh, where the plot doesn't
@@ -193,17 +203,27 @@ river_N_slider = Slider(title="River Nitrate level (mu mol/L)",
                         value=100., start=0., end=200., step=0.1)
 gas_exchange_slider = Slider(title="Gas Exchange Rate (m/day)",
                              value=3, start=1, end=5, step=2)
-productivity_slider = Slider(title="Biological Productivity (factor)",
+productivity_slider = Slider(title="Productivity (factor)",
                              value=1., start=0.5, end=2., step=0.5)
+
+# Potential interactive data table for inclusion
+columns = [
+    TableColumn(field="day", title="Day"),
+    TableColumn(field="S", title="Salinity"),
+    TableColumn(field="N", title="Nitrate"),
+    TableColumn(field="O", title="Oxygen")
+]
+data_table = DataTable(source=source, columns=columns,
+                       width=300, height=600)
 
 
 def toggle_callback(attr):
     if tide_toggle.active:
         # Checked *after* press
-        tide_toggle.label = "Disable tides"
+        tide_toggle.label = "Tides on"
     else:
-        tide_toggle.label = "Enable tides"
-tide_toggle = Toggle(label="Enable tides")
+        tide_toggle.label = "Tides off"
+tide_toggle = Toggle(label="Tides off")
 tide_toggle.on_click(toggle_callback)
 
 download_button = Button(label="Download plot data", callback=download_data)
@@ -215,8 +235,9 @@ go_button.on_click(update_plots)
 prods = VBox(gas_exchange_slider, productivity_slider)
 river = VBox(river_flow_slider, river_N_slider)
 tide_run = HBox(tide_toggle, download_button, go_button)
-all_settings = VBox(prods, river, tide_run)
+all_settings = VBox(prods, river, tide_run,
+                    width=300)
 
 # Add to current document
-curdoc().add_root(HBox(all_settings, plots, width=1400))
+curdoc().add_root(HBox(children=[all_settings, plots]))
 
